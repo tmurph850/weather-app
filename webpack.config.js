bc4ab8d6b8c50725083773263854ff4dca9c45bd
Tim Murphy <tmurph850@gmail.com>
2018-08-28 19:55:55 -0400

    Prep for re-design, diving back in!

--- webpack.config.js
+++ webpack.config.js
@@ -7,60 +7,60 @@ const webpack = require('webpack');
 const config = {
     entry: './src/index.js',
     output: {
-      filename: 'app.js',
-      path: path.resolve(__dirname, 'dist')
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
     },
     devtool: 'inline-source-map',
     devServer: {
-      contentBase: './dist',
-      hot: true,
-      port: 8080,
-      historyApiFallback: true,
-      publicPath: '/',
-      proxy: {
-        "/create": {
-          target: "http://localhost:3000",
-          pathRewrite: {"^/create" : ""},
-          secure: false,
-          changeOrigin: true
+        contentBase: './dist',
+        hot: true,
+        port: 8080,
+        historyApiFallback: true,
+        publicPath: '/',
+        proxy: {
+            "/create": {
+                target: "http://localhost:3000",
+                pathRewrite: {
+                    "^/create": ""
+                },
+                secure: false,
+                changeOrigin: true
+            }
         }
-      }
     },
     module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-                //allows for import of styles (import css from 'file.css');
-            }, {
-                test: /\.js$|\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        'presets': [
-                            [
-                                'env', {
-                                    'modules': false
-                                }
-                            ],
-                            'stage-2',
-                            'react',
-                        ]
-                    }
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+            //allows for import of styles (import css from 'file.css');
+        }, {
+            test: /\.js$|\.jsx$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    'presets': [
+                        [
+                            'env', {
+                                'modules': false
+                            }
+                        ],
+                        'stage-2',
+                        'react',
+                    ]
                 }
             }
-        ]
+        }]
     },
     mode: 'development',
     plugins: [
-      new HtmlWebpackPlugin({
-        title: 'MyWeather',
-        // Load a custom template (lodash by default see the FAQ for details)
-        template: './public/index.html'
-      }),
-     //new webpack.NamedModulesPlugin(),
-     new webpack.HotModuleReplacementPlugin()
+        new HtmlWebpackPlugin({
+            title: 'MyWeather',
+            // Load a custom template (lodash by default see the FAQ for details)
+            template: './public/index.html'
+        }),
+        //new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
 
